4e2a1df88d0f8f1f509d54348aefd8385f06747e
Robert Stevens <rstevens011@users.noreply.github.com>
2019-04-23 18:29:43 -0700

    add in support for assuming a IAM role

--- test/integ/test_aws_secretsmanager_caching.py
+++ test/integ/test_aws_secretsmanager_caching.py
@@ -12,17 +12,62 @@
 # language governing permissions and limitations under the License.
 import pytest
 import botocore
-import botocore.session
+import os
+from botocore.credentials import (
+    AssumeRoleCredentialFetcher,
+    CredentialResolver,
+    DeferredRefreshableCredentials,
+    JSONFileCache
+)
+from botocore.session import Session
 from aws_secretsmanager_caching.config import SecretCacheConfig
 from aws_secretsmanager_caching.secret_cache import SecretCache
+
+
+class AssumeRoleProvider(object):
+    METHOD = 'assume-role'
+
+    def __init__(self, fetcher):
+        self._fetcher = fetcher
+
+    def load(self):
+        return DeferredRefreshableCredentials(
+            self._fetcher.fetch_credentials,
+            self.METHOD
+        )
+
+
+def assume_role(session: Session, role_arn: str, duration: int = 3600,
+                session_name: str = None, serial_number: str = None) -> Session:
+
+    fetcher = AssumeRoleCredentialFetcher(session.create_client,
+                                          session.get_credentials(),
+                                          role_arn,
+                                          extra_args={
+                                            'DurationSeconds': duration,
+                                            'RoleSessionName': session_name,
+                                            'SerialNumber': serial_number
+                                          },
+                                          cache=JSONFileCache())
+    role_session = Session()
+    role_session.register_component(
+        'credential_provider',
+        CredentialResolver([AssumeRoleProvider(fetcher)])
+    )
+    return role_session
 
 
 class TestAwsSecretsManagerCachingInteg:
 
     @pytest.fixture(scope='module')
     def client(self):
-        session = botocore.session.get_session()
-        yield session.create_client('secretsmanager', region_name='us-west-2')
+        region = os.getenv('AWS_REGION', 'us-west-2')
+        base_session = botocore.session.get_session()
+        if os.environ.get('IAM_ROLE_ARN') is not None:
+            role_session = assume_role(base_session, role_arn=os.environ.get('IAM_ROLE_ARN'))
+            yield role_session.create_client('secretsmanager', region_name=region)
+        else:
+            yield base_session.create_client('secretsmanager', region_name=region)
 
     @pytest.fixture(scope='module')
     def secret_string(self, client):
